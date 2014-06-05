7f77b794951af9378302f4783190822092865b58
Espen Moe-Nilssen <espen@medialog.no>
2014-06-05 17:11:42 +0200

    updated gitignore

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = '0.1.1'
 
 long_description = (
     open('README.txt').read()
     + '\n' +
     'Contributors\n'
     '============\n'
-    + '\n' +
-    open('CONTRIBUTORS.txt').read()
     + '\n' +
     open('CHANGES.txt').read()
     + '\n')
